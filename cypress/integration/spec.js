d3d1bc3c540f33250ef89aff43f25bf22d6ad439
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-12-12 14:35:27 -0500

    chore: add cypress e2e tests, close #5

---
+++ cypress/integration/spec.js
@@ -0,0 +1,25 @@
+/* eslint-env mocha */
+/* global cy */
+describe('@cypress/snapshot', () => {
+  beforeEach(() => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  context('simple types', () => {
+    it('works with objects', () => {
+      cy.wrap({ foo: 42 }).snapshot()
+    })
+
+    it('works with numbers', () => {
+      cy.wrap(42).snapshot()
+    })
+
+    it('works with strings', () => {
+      cy.wrap('foo-bar').snapshot()
+    })
+
+    it('works with arrays', () => {
+      cy.wrap([1, 2, 3]).snapshot()
+    })
+  })
+})
---
+++ snapshots.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "@cypress/snapshot": {
+    "simple types": {
+      "works with objects": {
+        "1": {
+          "foo": 42
+        }
+      },
+      "works with numbers": {
+        "1": 42
+      },
+      "works with strings": {
+        "1": "foo-bar"
+      },
+      "works with arrays": {
+        "1": [
+          1,
+          2,
+          3
+        ]
+      }
+    }
+  },
+  "__version": "1.1.4"
+}
