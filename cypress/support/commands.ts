cba8b35abad851c894ae89f67134c1c013f3913f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-12-14 15:26:01 -0500

    checking that we can merge additional commands for typescript, close #9

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,2 +1,9 @@
 // register .snapshot() command
 require('../..').register()
+
+// merge Cypress interface with new command "snapshot()"
+declare namespace Cypress {
+  interface Chainable {
+    snapshot(): void
+  }
+}
